9273757c255ff123c69eecbe1417908334bec420
abhinay1-11 <abhinaya11171@gmail.com>
2026-05-24 16:13:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,27 +116,26 @@ elif page == "Transaction Explorer":
     st.title("🔎 Transaction Explorer")
     st.markdown("---")
 
-    with st.expander("👉 Don't know a TransactionID? Click here for samples"):
-        col1, col2, col3 = st.columns(3)
+with st.expander("👉 Don't know a TransactionID? Click here for samples"):
+    col1, col2, col3 = st.columns(3)
 
-        with col1:
-            st.markdown("🔴 **Critical Risk**")
-            critical_ids = results[results["risk_tier"] == "Critical Risk"]["TransactionID"].head(5).values
-            for tid in critical_ids:
-                st.code(tid)
+    with col1:
+        st.markdown("🔴 **Critical Risk**")
+        critical_ids = X_test[results["risk_tier"] == "Critical Risk"]["TransactionID"].head(5).values
+        for tid in critical_ids:
+            st.code(tid)
 
-        with col2:
-            st.markdown("🟡 **Suspicious**")
-            suspicious_ids = results[results["risk_tier"] == "Suspicious"]["TransactionID"].head(5).values
-            for tid in suspicious_ids:
-                st.code(tid)
+    with col2:
+        st.markdown("🟡 **Suspicious**")
+        suspicious_ids = X_test[results["risk_tier"] == "Suspicious"]["TransactionID"].head(5).values
+        for tid in suspicious_ids:
+            st.code(tid)
 
-        with col3:
-            st.markdown("🟢 **Clear**")
-            clear_ids = results[results["risk_tier"] == "Clear"]["TransactionID"].head(5).values
-            for tid in clear_ids:
-                st.code(tid)
-
+    with col3:
+        st.markdown("🟢 **Clear**")
+        clear_ids = X_test[results["risk_tier"] == "Clear"]["TransactionID"].head(5).values
+        for tid in clear_ids:
+            st.code(tid)
     search_id = st.text_input("Search by TransactionID", placeholder="e.g. 2987004")
 
     if search_id:
